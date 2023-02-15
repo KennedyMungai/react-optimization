f925a3a68a1b90258ae1404f386df8db46b07a0e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-15 14:50:37 +0300

    Added a console log to for the Demo Output component

--- src/components/Demo/DemoOutput.js
+++ src/components/Demo/DemoOutput.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const DemoOutput = (props) => {
-  return (
-    <p>{props.show ? 'This is new!' : ''}</p>
-  )
+    console.log("Demo Output")
+
+    return (
+        <p>{props.show ? 'This is new!' : ''}</p>
+    )
 }
 
 export default DemoOutput
